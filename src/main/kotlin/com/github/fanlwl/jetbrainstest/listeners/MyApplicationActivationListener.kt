302669182680351a12c5b0467963ecd56fc8bcff
GitHub Action <action@github.com>
2023-07-28 03:28:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/fanlwl/jetbrainstest/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/fanlwl/jetbrainstest/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.fanlwl.jetbrainstest.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
